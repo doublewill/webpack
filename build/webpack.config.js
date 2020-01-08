1c6c1929edfd885fd59da475c5b6b30be5ab052e
doublewill <xd8012@163.com>
2020-01-08 22:47:51 +0800

    add loaders

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
 	mode: 'development',
-	entry: path.resolve(__dirname, '../src/main.js'),
+	entry: ["@babel/polyfill",path.resolve(__dirname, '../src/main.js')],
 	output: {
 		filename: '[name].[hash:8].js',
 		path: path.resolve(__dirname, '../dist')
@@ -14,23 +14,79 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test:/\.js$/,
+				use:{
+						loader:'babel-loader',
+						options:{
+								presets:['@babel/preset-env']
+						}
+				},
+				exclude:/node_modules/
+	 	},					
+			{
 				test: /\.css$/,
 				use: ['style-loader', 'css-loader']
 			},
 			{
 				test: /\.less$/,
-				use: ['style-loader', 'css-loader', {
-					loader: 'postcss-loader',
-					options: {
-						plugins: [require('autoprefixer')]
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+			},
+			{
+				test: /\.(jpe?g|png|gif)$/i, //图片文件
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 10240,
+							fallback: {
+								loader: 'file-loader',
+								options: {
+									name: 'img/[name].[hash:8].[ext]'
+								}
+							}
+						}
 					}
-				}, 'less-loader']
-			},			
+				]
+			},
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 10240,
+							fallback: {
+								loader: 'file-loader',
+								options: {
+									name: 'media/[name].[hash:8].[ext]'
+								}
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 10240,
+							fallback: {
+								loader: 'file-loader',
+								options: {
+									name: 'fonts/[name].[hash:8].[ext]'
+								}
+							}
+						}
+					}
+				]
+			},
 		]
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
-			filename: '[name].hash.css',
+			filename: '[name].[hash:8].css',
 			chunkFilename: '[id].css'
 		}),
 		new HtmlWebpackPlugin({
