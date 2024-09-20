1eb11f7998edd12642629345c705afd8e0e32a89
doublewill <xd8012@163.com>
2024-09-20 10:50:06 +0800

    update webpack version

---
+++ webpack.config.js
@@ -0,0 +1,125 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { config } = require('webpack');
+
+const isProduction = process.env.NODE_ENV == 'production';
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:8].js',
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        hot: true,
+        port: 9000
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader'],
+            },
+            // {
+            //     test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+            //     type: 'asset',
+            // },
+            {
+                test: /\.(jpe?g|png|gif)$/i, //图片文件
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'img/[name].[hash:8].[ext]',
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //媒体文件
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'media/[name].[hash:8].[ext]',
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, // 字体
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'fonts/[name].[hash:8].[ext]',
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+        // new CleanWebpackPlugin(),
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+
+        config.plugins.push(new MiniCssExtractPlugin({
+            filename: '[name].[hash:8].css',
+            chunkFilename: '[id].css',
+        }));
+
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
